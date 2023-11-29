0835fa0e71384d99f014587e00a34ba6e747e01a
DuncanSmit01 <88308215+DuncanSmit01@users.noreply.github.com>
2023-11-29 14:37:50 +0200

    Update Player_Movement.cs
    Update

--- Fighting/Assets/Scripts/Player_Movement.cs
+++ Fighting/Assets/Scripts/Player_Movement.cs
@@ -7,9 +7,13 @@ public class Player_Movement : MonoBehaviour
     // Start is called before the first frame update
     public float Speed;
     public GameObject Avatar;
+<<<<<<< HEAD
     Animator PlayerAnim;
 
 
+=======
+    public Joystick joystick;
+>>>>>>> main
     void Start()
     {
         PlayerAnim = GetComponent<Animator>();
@@ -18,11 +22,20 @@ public class Player_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< HEAD
 
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         GetComponent<CharacterController>().Move(move * Time.deltaTime * Speed);
 
+
         Vector3 MovementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+=======
+       
+        Vector3 move = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+        GetComponent<CharacterController>().Move(move * Time.deltaTime * Speed);
+
+        Vector3 MovementDirection = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+>>>>>>> main
         this.transform.Translate(MovementDirection*Speed*Time.deltaTime,Space.World);
 
        
